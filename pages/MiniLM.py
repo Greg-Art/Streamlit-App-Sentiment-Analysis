5db41472b7e7abc1f26931fac497a509d9b08efb
Greg-Art <gregoryarthur98@gmail.com>
2023-07-24 18:49:39 +0000

    update

--- pages/MiniLM.py
+++ pages/MiniLM.py
@@ -10,56 +10,55 @@ from scipy.special import softmax
 from transformers import pipeline
 import xformers
 
-
-st.title("Welcome to the MiniLM Model Page")
-
-
-@st.cache_data
+## Creating a cache to store my model for efficiency
+@st.cache_data(ttl=86400)
 def load_model(model_name):
-    model= AutoModelForSequenceClassification.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
     return model
 
-##creating my model
-ml_model= load_model("gArthur98/Greg-Sentiment-classifier")
-
-@st.cache_data
+## Creating my tokenizer
+@st.cache_data(ttl=86400)
 def load_tokenizer(tokenizer_name):
-    tokenizer= AutoTokenizer.from_pretrained(tokenizer_name)
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
     return tokenizer
 
-ml_token= load_tokenizer("gArthur98/Greg-Sentiment-classifier") 
+## Front end
+st.title("Welcome to the MiniLM Model Page")
 
-##front end 
+text = st.text_input("Please Enter Your Sentence Below: ")
 
-text= st.text_input("Please Enter Your Sentence Below: ")
-
-##cleaning
-
+## Cleaning
 def data_cleaner(text):
-    text= text.lower()
-     ##removing has tags
-    text= re.sub(r'#\w+', '', text)
-  ##removing punctuations
-    text= re.sub("[^\w\s]", repl= "", string=text)
-    text= re.sub(r'\d+', '', text)
-    text= " ".join([word for word in text.split() if not word.isdigit()]) ##removing digits
+    text = text.lower()
+    ## Removing hashtags
+    text = re.sub(r'#\w+', '', text)
+    ## Removing punctuations
+    text = re.sub("[^\w\s]", repl="", string=text)
+    text = re.sub(r'\d+', '', text)
+    text = " ".join([word for word in text.split() if not word.isdigit()])
     return text
 
-##running my input through my function
+## Running my input through my function
+input_text = data_cleaner(text)
 
-input_text= data_cleaner(text)
+if 'ml_model' not in st.session_state:
+    st.session_state.ml_model = load_model("gArthur98/Greg-Sentiment-classifier")
 
-pipe= pipeline("text-classification", model=ml_model, tokenizer=ml_token)
+if 'ml_token' not in st.session_state:
+    st.session_state.ml_token = load_tokenizer("gArthur98/Greg-Sentiment-classifier")
 
-result= pipe(input_text)
+pipe = pipeline("text-classification", model=st.session_state.ml_model, tokenizer=st.session_state.ml_token)
+
+result = pipe(input_text)
 
 final = st.button("Calculate Sentiment")
 
 if final:
     for results in result:
-        if results['label']== 'LABEL_0':
+        if results['label'] == 'LABEL_0':
             st.write(f"Your sentiment is Negative with a confidence score of {results['score']}")
-        elif results["label"]=='LABEL_1':
+        elif results["label"] == 'LABEL_1':
            st.write(f"Your sentiment is Neutral with a confidence score of {results['score']}")
         else:
            st.write(f"Your sentiment is Positive with a confidence score of {results['score']}")
+
