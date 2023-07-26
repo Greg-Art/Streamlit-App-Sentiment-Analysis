8e2eeaef99ed145465a41d1922619314ccc186f4
Greg-Art <gregoryarthur98@gmail.com>
2023-07-26 10:04:55 +0000

    update

--- pages/2_Roberta.py
+++ pages/2_Roberta.py
@@ -50,11 +50,11 @@ st_lottie(lottie_hello, height= 200) ##loading my gif
 
 text = st.text_input("Please Enter a Covid-19 Themed Sentence Below: ")
 
-st.write("""Example of sentences /n
+st.write("""Example of sentences to input:
          
-         - I hate the vaccine /n
-         - I love the vaccine /n
-         - Covid-19 is Moving Fast
+         - I hate the vaccine \n
+    - I love the vaccine \n
+    - Covid-19 is Moving Fast
     
          """)
 
